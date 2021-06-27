72a199824f7411053c0d9609a70f11ef14a3835d
nikhil <nikhil.gosavi@pentacor.de>
2021-06-27 11:09:53 +0200

    initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+const faker = require('faker');
+
+let cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+document.querySelector('#dev-cart').innerHTML = cartText;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWekpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        port: 8082
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'cart',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './CartShow': './src/index'
+            },
+            shared: ['faker'],
+            /**some modules like React cannot be loaded multiple times, therefore singleton modules
+             * this will load only one copy 
+            shared: {
+                faker: {
+                    singleton: true
+                }
+            },
+            // singleton modules end */
+        }), 
+       new HtmlWekpackPlugin({
+           template: './public/index.html'
+       })
+    ]
+}
